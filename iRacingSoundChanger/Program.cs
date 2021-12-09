52d3a3f2ad2ec2bfe58dd6d1f7724f5f21fd1c59
Chris Rae <clr@chrisrae.com>
2021-12-08 20:14:56 -0800

    Parsing ini files myself instead of using library

--- iRacingSoundChanger/Program.cs
+++ iRacingSoundChanger/Program.cs
@@ -1,12 +1,9 @@
 ﻿using System;
 using System.Diagnostics;
 using System.IO;
+using System.Management;
 using System.Text.RegularExpressions;
 using CommandLine;
-using IniParser;
-using IniParser.Model;
-using IniParser.Model.Configuration;
-using IniParser.Parser;
 
 namespace iRacingSoundChanger
 {
@@ -21,6 +18,31 @@ namespace iRacingSoundChanger
             public string OutputVoice { get; set; }
         }
 
+        private static string iniGetValue(string[] inif, string name)
+        {
+            for (int i = 0; i < inif.Length; i++)
+            {
+                string prefix = name + "=";
+                if (inif[i].StartsWith(prefix))
+                {
+                    return inif[i].Substring(prefix.Length);
+                }
+            }
+            throw new Exception($"Can't find INI file entry for {name}");
+        }
+
+        private static void iniSetValue(string[] inif, string name, string newVal)
+        {
+            for (int i = 0; i < inif.Length; i++)
+            {
+                string prefix = name + "=";
+                if (inif[i].StartsWith(prefix))
+                {
+                    inif[i] = prefix + newVal;
+                }
+            }
+        }
+
         static void Main(string[] args)
         {
             var docs = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
@@ -30,31 +52,34 @@ namespace iRacingSoundChanger
             {
                 string appIni = Path.Combine(iRacingDocs, "app.ini");
                 string appIniBak = Path.Combine(iRacingDocs, "app.ini.bak");
-                Console.WriteLine($"Saving current INI file as {appIniBak}");
+                Console.WriteLine($"Saving current INI file as {appIniBak}. To use the current main audio and voice audio, type:");
+                Console.WriteLine();
+
+                string[] iniContents = File.ReadAllLines(appIni);
+                string curMainD = iniGetValue(iniContents, "devSpeakerId");
+                curMainD = Regex.Replace(curMainD, @"\;.*", "").Trim();
+                string curVoiceD = iniGetValue(iniContents, "devVoiceChatId");
+                curVoiceD = Regex.Replace(curVoiceD, @"\;.*", "").Trim();
+                Console.WriteLine($"iRacingSoundChanger -m {curMainD} -v {curVoiceD}");
+
                 File.Copy(appIni, appIniBak, true);
 
-                //var pconf = new IniData();
-                //pconf.Configuration.AssigmentSpacer = "";
-                var parser = new FileIniDataParser(new IniDataParser(new IniParserConfiguration() { AssigmentSpacer = "" }));
-                IniData data = parser.ReadFile(appIni);
                 if (o.OutputMain != null)
                 {
-                    string curMainD = data["Audio"]["devSpeakerName"];
                     // Sometimes this ends in comments
-                    curMainD = Regex.Replace(curMainD, @"\;.*", "").Trim();
-                    Console.WriteLine($"Changing main audio device from {curMainD} to {o.OutputMain}");
-                    data["Audio"]["devSpeakerName"] = o.OutputMain;
+                    Console.WriteLine($"Setting main audio device to {o.OutputMain}");
+                    iniSetValue(iniContents, "devSpeakerId", o.OutputMain);
+                    iniSetValue(iniContents, "devSpeakerName", "tbd");
                 }
 
                 if (o.OutputVoice != null)
                 {
-                    string curVoiceD = data["Audio"]["devVoiceChatName"];
                     // Sometimes this ends in comments
-                    curVoiceD = Regex.Replace(curVoiceD, @"\;.*", "").Trim();
-                    Console.WriteLine($"Changing voice audio device from {curVoiceD} to {o.OutputVoice}");
-                    data["Audio"]["devVoiceChatName"] = o.OutputVoice;
+                    Console.WriteLine($"Setting voice audio device to {o.OutputVoice}");
+                    iniSetValue(iniContents, "devVoiceChatId", o.OutputVoice);
+                    iniSetValue(iniContents, "devVoiceChatName", "tbd");
                 }
-                parser.WriteFile(appIni, data);
+                File.WriteAllLines(appIni, iniContents);
             });
         }
     }
